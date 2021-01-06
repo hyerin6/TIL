dd01d1414ed1ca747c655383b60caf9f63aa415d
hyerin6 <hyerinn6@gmail.com>
2021-01-06 23:09:43 +0900

    Add aop example1

---
+++ aop_example/aop_ex1/src/test/java/com/test/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
